fff1ed79dd8f1398887db99ca8168798c6365c63
Bryan Reategui <bryan.rr@hotmail.com>
2026-04-18 09:40:29 -0500

    chore: initial Spring Boot scaffolding

---
+++ src/main/java/com/brr/customers/api/CustomersApiApplication.java
@@ -0,0 +1,13 @@
+package com.brr.customers.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApiApplication.class, args);
+	}
+
+}
